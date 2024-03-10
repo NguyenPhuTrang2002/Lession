2c49231a93c7f86bb94d588fa050a644c13c7d2e
NguyenPhuTrang2002 <trangnp@tokyotechlab.com>
2024-03-10 23:26:40 +0700

    update ui

--- project/src/pages/Home/Index.js
+++
@@ -1,9 +0,0 @@
-import Index from '../../components/LessionOne/Index';
-function Home() {
-  return (
-    <div className="min-h-screen overflow-y-auto background pb-[250px]">
-      <Index />
-    </div>
-  )
-}
-export default Home;
